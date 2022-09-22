fefbc9e5a44d6ca6c3601f5ad3417c80af71140c
Jack Ta <jack.th.ta@gmail.com>
2022-09-21 18:40:36 -0700

    style: lint
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components()],
+  plugins: [vue(), Components()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
