ae894edf0185c93d61689548cac9795714e9158a
Jack Ta <jack.th.ta@gmail.com>
2022-09-23 18:12:45 -0700

    feat: finish scaffolding `FileUpload` component functionality
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
+import { parseFileContent } from "@/utilities/file";
+
+import type { Employees } from "@/types/employee";
+
+const emit = defineEmits<{ (e: 'upload', employees: Employees): void }>();
 
 const fileReader = new FileReader();
 const acceptedFileTypes = "text/csv";
@@ -8,10 +13,11 @@ onMounted(() => {
   fileReader.onload = (e: any) => {
     // on successful read of the file (triggered with `fileReader.readAsText()`),
     // file content text will exist in `e.target.result`
-    const fileText = e.target.result;
+    const fileContent = e.target.result;
 
-    // NOTE: need to decide whether to parse text here (i.e., convert text to array of objects)
-    // OR do it in parent component (via emitting `fileText`). Maybe make a `FileService`?
+    const employees = parseFileContent(fileContent);
+
+    emit('upload', employees);
   };
 });
 
@@ -25,18 +31,9 @@ function onFileUpload(e: any) {
 </script>
 
 <template>
-  <label for="upload">Upload .csv</label>
-  <input
-    type="file"
-    id="upload"
-    :accept="acceptedFileTypes"
-    @change="onFileUpload"
-  />
-</template>
+  <label class="inline-block border rounded-sm px-4 py-1" for="upload">
+    <span>Upload File</span>
+    <input type="file" id="upload" class="hidden" :accept="acceptedFileTypes" @change="onFileUpload" />
+  </label>
 
-<style>
-/* TODO: Remove when styling with Tailwind CSS */
-input {
-  display: none;
-}
-</style>
+</template>
