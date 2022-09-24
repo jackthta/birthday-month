83d6e255fb6ffed0c7d7ef65cf6df32dbb2b33a5
Jack Ta <jack.th.ta@gmail.com>
2022-09-23 17:25:47 -0700

    feat: create `file` utility
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

---
+++ src/utilities/file.ts
@@ -0,0 +1,29 @@
+import type { Employees } from "@/types/employee";
+
+/**
+ * Parse and transform file contents into an array of `Employee`.
+ */
+export function parseFileContent(fileContent: string): Employees {
+  // split monolithic string of employee data by carriage return and/or new line
+  const employees = fileContent
+    .split(/\r?\n/)
+
+    // remove quotation marks and white space
+    .map((employee) => employee.replaceAll('"', "").trim())
+
+    // split string of employee data by comma
+    .map((employee) => employee.split(","))
+
+    // mold into Employee object
+    .map((employee) => {
+      const [name, ...birthdayRaw] = employee;
+
+      // raw birthday is in the form "MM DD, YYYY". the comma
+      // will be parsed and split. place comma back to have valid date.
+      const birthday = new Date(birthdayRaw.join(","));
+
+      return { name, birthday };
+    });
+
+  return employees;
+}
