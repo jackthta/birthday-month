264b784169be5e88482605cd41674bbfe0d8e181
Jack Ta <jack.th.ta@gmail.com>
2022-09-26 19:21:27 -0700

    chore: add reminder to lint tailwind classes
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+<!-- TODO: need to figure out programmatic way to lint tailwind classes to organize them better -->
+
 <template>
   <main
     class="h-screen max-h-screen flex flex-col justify-center items-center bg-[url('./assets/wave-bg.svg')] bg-no-repeat bg-center bg-cover"
